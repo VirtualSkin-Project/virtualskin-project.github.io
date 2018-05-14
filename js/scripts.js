575a64158b11efa6294143cf10b598b10f0b0ee6
Jonathan LOQUET <jonthan.lt@gmail.com>
2018-05-14 10:13:20 +0200

    Subscribe to newsletter

--- js/scripts.js
+++ js/scripts.js
@@ -31,4 +31,26 @@
        $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
     });
 
+    $('#alertModal').on('shown.bs.modal', function () {
+        $(this).off('submit', '#emailForm').on('submit', '#emailForm', function (event) {
+            event.preventDefault();
+            var modal = $(this).closest('.modal');
+            var name = $('.modal-body #name').val();
+            var email = $('.modal-body #email').val();
+            var phone = $('.modal-body #phone').val();
+            var msg = $('.modal-body #msg').val();
+            $.ajax({
+                method: "POST",
+                url: "https://virtualskin-mail.herokuapp.com/add",
+                data: {name: name, email: email, phone: phone, msg: msg}
+            }).done(function (msg) {
+                if (msg && msg.status)
+                    toastr.success("Thanks, it was written in our hearts forever");
+                else
+                    toastr.warning("Oh no, we lost your address");
+            });
+            $(modal).modal('hide');
+        });
+    });
+
 })(jQuery);
